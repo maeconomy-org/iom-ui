bce97b647a70b63c2f1a4bf93937dd985c92cd87
dev-maeconomy <dev@maeconomy.org>
2025-12-11 23:44:03 +0200

    patch next/reactjs versions, minor fixes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,7 +11,7 @@ export const AUTH_SESSION_TIMEOUT = 12 * 60 * 60 * 1000 // 12 hours in milliseco
 export const NAV_ITEMS = [
   { name: 'Objects', path: '/objects' },
   { name: 'I/O Processes', path: '/processes' },
-  { name: 'Groups', path: '/groups' },
+  // { name: 'Groups', path: '/groups' },
   { name: 'Models', path: '/models' },
   { name: 'Import', path: '/import' },
   // { name: 'API Documentation', path: '#', isDisabled: true },
